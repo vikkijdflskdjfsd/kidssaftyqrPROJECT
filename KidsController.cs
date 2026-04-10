2aa066af9886b454bbd2ad37cc95557a79ed9553
vikkijdflskdjfsd <67059071+vikkijdflskdjfsd@users.noreply.github.com>
2026-04-10 17:36:45 +0530

    Add files via upload

---
+++ KidsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KidsSafetyQR.Data;
+using KidsSafetyQR.Models;
+
+public class KidsController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public KidsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Dashboard()
+    {
+        try
+        {
+            var kids = _context.Kids.ToList();
+            return View(kids);
+        }
+        catch (Exception ex)
+        {
+            return Content("ERROR: " + ex.Message);
+        }
+    }
+
+    public IActionResult Add()
+    {
+        return View(); // 👈 ye important hai
+    }
+
+    [HttpPost]
+    [HttpPost]
+    [HttpPost]
+    public IActionResult Add(Kid kid)
+    {
+        kid.Code = Guid.NewGuid().ToString(); // 👈 UNIQUE CODE
+        kid.IsLost = false;
+
+        _context.Kids.Add(kid);
+        _context.SaveChanges();
+
+        return RedirectToAction("Dashboard");
+    }
+}
